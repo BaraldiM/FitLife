aeae8db0c5a942fbb62b905b2bb4b3cbf7f27c1f
Edson Henrique Baraldi <72748677+BaraldiM@users.noreply.github.com>
2023-10-31 19:49:38 -0300

    IMC
    criação da tela de IMC

---
+++ app/src/main/java/co/Baraldi/FitLife/ImcActivity.kt
@@ -0,0 +1,11 @@
+package co.Baraldi.FitLife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
--- app/src/main/java/co/Baraldi/FitLife/MainActivity.kt
+++ app/src/main/java/co/Baraldi/FitLife/MainActivity.kt
@@ -2,10 +2,22 @@ package co.Baraldi.FitLife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import co.Baraldi.FitLife.R.layout.activity_main
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnImc: LinearLayout // BOTÃO IMC
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+        btnImc.setOnClickListener{
+               val i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+            //NAVEGAR ENTRE AS TELAS
+        }
     }
 }
