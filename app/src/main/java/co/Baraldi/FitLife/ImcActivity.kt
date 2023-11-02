b492712ebdd7c2c617d5f2ae97d7db871ddaef77
Edson Henrique Baraldi <72748677+BaraldiM@users.noreply.github.com>
2023-11-02 19:03:14 -0300

    Validate
    inclusão validade

--- app/src/main/java/co/Baraldi/FitLife/ImcActivity.kt
+++ app/src/main/java/co/Baraldi/FitLife/ImcActivity.kt
@@ -2,10 +2,42 @@ package co.Baraldi.FitLife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_messages, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+        }
     }
+
+    private fun validate(): Boolean {
+        //Não inserir valor menor ou igual a o
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
+    }
+
+
+
+
+
 }
