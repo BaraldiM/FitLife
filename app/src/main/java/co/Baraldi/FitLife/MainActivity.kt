01e3f88e145ebcde9ccd49b222485fd2d6a188ef
Edson Henrique Baraldi <72748677+BaraldiM@users.noreply.github.com>
2023-11-06 21:11:14 -0300

    Metodos RecyclerView

--- app/src/main/java/co/Baraldi/FitLife/MainActivity.kt
+++ app/src/main/java/co/Baraldi/FitLife/MainActivity.kt
@@ -5,19 +5,42 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import co.Baraldi.FitLife.R.layout.activity_main
 import android.content.Intent
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnImc: LinearLayout // BOTÃO IMC
+
+    private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
 
-        btnImc = findViewById(R.id.btn_imc)
-        btnImc.setOnClickListener{
-               val i = Intent(this, ImcActivity::class.java)
-            startActivity(i)
-            //NAVEGAR ENTRE AS TELAS
+        val adapter = MainAdapter()
+        rvMain = findViewById(R.id.rv_main)
+        rvMain.adapter = adapter
+
+
+
+    }
+
+    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            TODO("Not yet implemented")
         }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    private class MainViewHolder (view: View) : RecyclerView.ViewHolder(view){
+
     }
 }
